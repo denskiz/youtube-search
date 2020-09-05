e30b6c2d9f9ace1e6e0fb945ede70e8552c4b0e4
dennis <dennisent@protonmail.com>
2020-09-05 13:13:54 +1000

    Updates

---
+++ precache-manifest.136491ba0092a710b222472796f16bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060d30554a5ea12e91187f580b7b0009",
+    "url": "/youtube-search/index.html"
+  },
+  {
+    "revision": "514908f9e5ff051f25d9",
+    "url": "/youtube-search/static/css/main.32598615.chunk.css"
+  },
+  {
+    "revision": "0464f8fe30748517b05c",
+    "url": "/youtube-search/static/js/2.9a6db237.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-search/static/js/2.9a6db237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514908f9e5ff051f25d9",
+    "url": "/youtube-search/static/js/main.628a620f.chunk.js"
+  },
+  {
+    "revision": "8565e40cf071b3d6d1ec",
+    "url": "/youtube-search/static/js/runtime-main.6f6815d1.js"
+  }
+]);
